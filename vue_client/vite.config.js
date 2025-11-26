47f710b0d116159e6a2b7164ba663feb753d5c00
Brad Root <bradroot@me.com>
2025-11-25 21:27:52 -0800

    add github actions for pushing demo mode to github pages

--- vue_client/vite.config.js
+++ vue_client/vite.config.js
@@ -3,9 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 const isDemo = process.env.VITE_DEMO_MODE === 'true'
+const base = process.env.VITE_BASE_PATH || '/'
 
 export default defineConfig({
   plugins: [vue()],
+  base: base,
   resolve: {
     alias: {
       '@api': path.resolve(__dirname, isDemo ? 'src/api/demo/index.js' : 'src/api/client.js')
